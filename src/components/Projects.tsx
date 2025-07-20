7bbb88050516c2611c6e99ae3acc96c7a6df3051
Filipe Klinger <filipeklinger@gmail.com>
2025-07-19 22:20:14 -0300

    feat: add new project details and images for Arbi, Tim Live, BTG Campus, and CAC in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,18 +3,39 @@ import React from 'react';
 // Exemplo de dados de projetos
 const projects = [
   {
+    title: 'Arbi - Landing Page',
+    description: 'Landing page desenvolvida para o banco Arbi, com foco em apresentar os serviços e soluções da instituição. O design é responsivo e otimizado para navegação em dispositivos móveis e desktops. Este projeto foi realizado enquanto atuo como desenvolvedor na TG4.',
+    tech: ['React'],
+    image: '/img/arbi.webp',
+    systemLink: 'https://www.bancoarbi.com.br/',
+  },
+  {
+    title: 'Tim Live - Carrinho de Compras',
+    description: 'Criação do backend para o carrinho de compras da plataforma Tim Live, utilizando .Net C#. O sistema permite que os usuários selecionem e adquiram serviços de internet de forma eficiente e segura. Este projeto foi desenvolvido enquanto atuo como desenvolvedor na TG4.',
+    tech: ['.Net C#', 'MySQL'],
+    image: '/img/timlive.webp',
+    systemLink: 'https://internet.tim.com.br/',
+  },
+  {
+    title: 'BTG Campus - Plataforma de Cursos internos',
+    description: 'Plataforma de cursos internos desenvolvida para o BTG Pactual, com o objetivo de oferecer treinamentos e capacitações aos colaboradores. A plataforma é responsiva e permite a gestão de cursos, usuários e relatórios de desempenho. Este projeto foi realizado enquanto atuo como desenvolvedor na TG4.',
+    tech: ['.Net C#', 'Angular', 'MongoDB'],
+    image: '/img/btgcampus.webp',
+    systemLink: '',
+  },
+  {
     title: 'Anfope',
     description: 'O sistema Anfope foi desenvolvido para facilitar a gestão de associados, professores, estudantes e demais participantes da organização. Seu principal objetivo é centralizar e automatizar processos como cadastro, controle de acesso, emissão de certificados, gerenciamento de anuidades, participação em eleições internas e geração de relatórios.',
-    tech: ['Html + Js', 'PHP'],
-    image: '/img/anfope.webp', // Exemplo de imagem
+    tech: ['Html + Js', 'PHP', 'MariaDB'],
+    image: '/img/anfope.webp',
     systemLink: 'https://www.anfope.org.br/sistema/',
   },
   {
-    title: 'Projeto 2',
-    description: 'Descrição breve do projeto 2.',
-    tech: ['.NET', 'Angular'],
-    image: '/img/cornered-stairs.svg',
-    systemLink: 'https://exemplo.com/projeto2',
+    title: 'Centro de Arte e Cultura (CAC - UFRRJ)',
+    description: 'O Centro de Arte e Cultura (CAC) da UFRRJ é um espaço dedicado à promoção e difusão da cultura, oferecendo uma variedade de atividades artísticas e culturais para a comunidade.',
+    tech: ['Html + Js', 'PHP', 'MySQL'],
+    image: '/img/cac.webp',
+    systemLink: 'https://r1.ufrrj.br/dac/',
   },
 ];
 
